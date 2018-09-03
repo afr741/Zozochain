607ef6307acfc74f92950e2028005bcca47c869d
Anushervon Rakhmatov <afr741@mun.ca>
2018-09-02 22:04:01 -0230

    additional classes added

---
+++ Block.java
@@ -0,0 +1,34 @@
+import java.util.Date;
+
+
+public class Block {
+	
+	
+
+ public String hash;
+ public String previousHash;
+ private String data;
+ private long timeStamp;
+ 
+ 
+//block constructor
+ 
+ 
+ public Block(String data, String previousHash ) {
+	this.data = data;
+	this.previousHash = previousHash;
+	this.timeStamp = new Date().getTime();
+	this.hash = calculateHash(); //Making sure we do this after we set the other values.
+ }
+ 
+ 
+ public String calculateHash() {
+		
+		String calculatedhash = StringUtil.applySha256(
+				previousHash +
+				Long.toString(timeStamp) +
+				data
+				);
+		return calculatedhash;
+	}
+ }
--- ZozoChain.java
+++ ZozoChain.java
@@ -1,23 +1,17 @@
-import java.util.Date;
-
 
 public class ZozoChain {
-	
-	
-
- public String hash;
- public String previousHash;
- private String data;
- private long timeStamp;
- 
- 
-//block contructor
- 
- 
- public ZozoChain(String data, String previousHash ) {
-	this.data = data;
-	this.previousHash = previousHash;
-	this.timeStamp = new Date().getTime();
-	
+ public static void main(String[] args) {
+	 
+	 Block genesisBlock  = new Block("Hi i am the first block", "0");
+	 System.out.println("Hash for block 1: " + genesisBlock.hash);
+	 
+	 Block secondBlock = new Block("Hi i am the second block", genesisBlock.hash);
+	 System.out.println("Hash for block 2: " + secondBlock.hash);
+	 
+	 
+	 Block thirdBlock = new Block("Hi i am the third block", secondBlock.hash);
+	 System.out.println("Hash for block 2: " + thirdBlock.hash);
+	 
+	 
  }
- }
+}
